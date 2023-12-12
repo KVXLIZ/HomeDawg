9902fe9cbeeb07b929d3411ca4da31ec07e6b9da
KVXLIZ <70652179+KVXLIZ@users.noreply.github.com>
2023-12-12 09:11:50 +0100

    added dawg

--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
+import requests
+import socket
 from .models import Air, Cam, Dist
 
 views = Blueprint('views', __name__)
@@ -9,3 +11,10 @@ def home():
     #pic_ID = Cam.query.order_by(Cam.id.desc()).first().id
     #dt = Dist.query.order_by(Dist.id.desc()).first().dist
     return render_template("index.html")
+
+@views.route('/button')
+def button():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.connect(('192.168.1.30', '5001'))
+    sock.sendall('4')
+    return redirect(url_for('/'))
