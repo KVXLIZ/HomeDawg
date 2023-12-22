f5a1de7dfd8d90935db408253cd3278b40a481c6
KVXLIZ <70652179+KVXLIZ@users.noreply.github.com>
2023-12-22 11:35:59 +0100

    Presence detection, camera feeds added

---
+++ presence.py
@@ -0,0 +1,25 @@
+import requests, socket
+from time import sleep
+
+started = False
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+while True:
+    r = requests.get('http://192.168.1.60/home/connected')
+
+    data = r.json()
+    if len(data['up']) == 0:
+        if not started:
+            sock.connect(('192.168.1.30', 5001))
+            sock.sendall(b'start')
+            started = True
+            print('started')
+            requests.get('http://192.168.1.60/surveilance/newround')
+        sleep(15)
+    else:
+        if started:
+            sock.connect(('192.168.1.30', 5001))
+            sock.sendall(b'finis')
+            print('finised')
+            started = False
+        sleep(15)
--- website/cam.py
+++ website/cam.py
@@ -1,21 +1,38 @@
-from flask import Blueprint, Response, request, jsonify
+from flask import Blueprint, Response, request, render_template
 from . import db
 from .models import Cam
 import numpy as np
 from PIL import Image
-from os import remove
+from os import remove, mkdir
 import cv2
 import glob
 from time import sleep
 
 cam = Blueprint('cam', __name__)    
         
+pic_ID = 0
 
-@cam.route('/')
-def surveilance():
+@cam.route('/', methods = ['GET'])
+def root():
+    if request.args.get('feed') is not None:
+        feed = request.args.get('feed')
+    else:
+        elements = db.session.query(Cam).all()
+        if elements == None:
+            feed = 0
+        else:
+            feed = len(elements)
+            print(len(elements))
+    return render_template('surveilance.html', rng=int(feed))
+
+@cam.route('/data/get', methods=['GET'])
+def feed():
+    if request.args.get('num') is not None:
+        folder = request.args.get('num')
+    else:
+        folder = db.session.query(Cam).order_by(Cam.id.desc()).first().id
     def generate_frames():
-        print(glob.glob("*"))
-        imgs = [cv2.imread(file) for file in glob.glob("/home/pi/HomeDawg/website/static/pics/*.jpg")]
+        imgs = [cv2.imread(file) for file in glob.glob(f"/home/pi/HomeDawg/website/static/cam{folder}/*.jpg")]
         imgs = [cv2.resize(img, (320, 240), fx = 0.5, fy = 0.5) for img in imgs]
         frames = [cv2.imencode('.jpg', img)[1].tobytes() for img in imgs]
         while True:
@@ -24,19 +41,29 @@ def surveilance():
                 sleep(1) 
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@cam.route('/data', methods=['GET', 'POST'])
+
+@cam.route('/newround', methods=['GET'])
+def newround():
+    global pic_ID
+    pic_ID = 0
+    element = Cam()
+    db.session.add(element)
+    db.session.commit()
+    mkdir(f'/home/pi/HomeDawg/website/static/cam{element.id}')
+    return '200'
+
+
+@cam.route('/data/add', methods=['GET', 'POST'])
 def data():
+    global pic_ID
     if request.method == 'POST':
-        element = Cam()
-        db.session.add(element)
-        db.session.commit()
-        pic_ID = element.id
+        folder = db.session.query(Cam).order_by(Cam.id.desc()).first().id
         width = 160
         height = 120
         buf = bytearray(2 * width * height)
         row = np.empty((width), int)
         buf = request.data
-        filename = 'HomeDawg/website/static/pics/pic.ppm'
+        filename = '/home/pi/HomeDawg/website/static/pics/pic.ppm'
         file = open(filename, 'w')
         file.write(f'P3 {width} {height} 255\n')
         file.close()
@@ -51,9 +78,10 @@ def data():
                     f.write(str(e)+' ')
                     f.write(str(e)+'\n')
         
-        im = Image.open('HomeDawg/website/static/pics/pic.ppm')
-        remove('HomeDawg/website/static/pics/pic.ppm')
-        im.save(f'HomeDawg/website/static/pics/pic{pic_ID}.jpg')
+        im = Image.open('/home/pi/HomeDawg/website/static/pics/pic.ppm')
+        remove('/home/pi/HomeDawg/website/static/pics/pic.ppm')
+        im.save(f'/home/pi/HomeDawg/website/static/cam{folder}/pic{pic_ID}.jpg')
+        pic_ID+=1
         return '200'
     elif request.method == 'GET':
         if request.args.get('secret') == 'balls':
